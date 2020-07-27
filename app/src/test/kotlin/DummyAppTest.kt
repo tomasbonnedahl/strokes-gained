aaf57b9455c77f5fdc33dc8427168be0f7511ae9
Tomas Bonnedahl <bonnedahl@gmail.com>
2020-07-27 15:55:57 +0200

    Add app module

---
+++ app/src/test/kotlin/DummyAppTest.kt
@@ -0,0 +1,9 @@
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.Test
+
+class DummyAppTest {
+    @Test
+    fun `test dummy`() {
+        assertThat(1.0).isEqualTo(1.0)
+    }
+}
