b06df76815eb3bfb528b491beac1251607a1ab94
Tomas Bonnedahl <bonnedahl@gmail.com>
2020-07-27 15:49:23 +0200

    Main fun and tests running in domain module

--- domain/src/test/kotlin/DummyDomainTest.kt
+++ domain/src/test/kotlin/DummyDomainTest.kt
@@ -1,6 +1,9 @@
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.Test
+
 class DummyDomainTest {
-//    @Test
-//    fun `test dummy to get dependencies fixed`() {
-//        assertThat(1.0).isEqualTo(1.0)
-//    }
+    @Test
+    fun `test dummy to get dependencies fixed`() {
+        assertThat(1.0).isEqualTo(1.0)
+    }
 }
